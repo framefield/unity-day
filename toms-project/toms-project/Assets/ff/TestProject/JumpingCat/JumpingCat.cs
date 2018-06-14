60068ea8a13b387a49926254440c51769573077b
Thomas Mann <thomas.mann@framefield.com>
2018-06-14 16:24:11 +0200

    Added basic light setup

---
+++ toms-project/toms-project/Assets/ff/TestProject/JumpingCat/JumpingCat.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ff.TestProject.cat
+{
+    /** Quick test implementation for cat interaction */
+    public class JumpingCat : MonoBehaviour
+    {
+        [Range(0, 10)]
+        public float JumpHeight;
+
+        [Range(0, 10)]
+        public float JumpFrequency;
+
+        public void IncreaseJumpHeight()
+        {
+            JumpHeight = JumpHeight + 1;
+        }
+
+        void Update()
+        {
+            var jumpHeight = Mathf.Pow(Mathf.Sin(Time.time * JumpFrequency), 2) * JumpHeight;
+            transform.localPosition = Vector3.up * jumpHeight + Vector3.forward * Time.time;
+        }
+
+    }
+}
